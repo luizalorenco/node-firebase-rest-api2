57667b9634cb6fe60d947746eabd6d1d11054c55
luizalorenco <luiza.lorenco@aluno.unc.br>
2022-10-19 21:57:20 -0300

    aula 10

--- bin/base/repository-base.js
+++ bin/base/repository-base.js
@@ -1,31 +1,53 @@
 // definindo a utilização do 'strict' mode
 'use strict'
 
-// definindo os 'imports'
+// Definindo os 'imports'
 const firebase = require('../../db')
 const firestore = firebase.firestore()
 
 class repositoryBase {
-    constructor(model) {
-        this._model = model
-    }
+  constructor(model, collection) {
+    this._model = model
+    this._collection = collection
+  }
 
-    modelBase = require('../../models/' + this._model)
+  async create(data) {
+    return await firestore.collection(this._collection).doc().set(data)
+  }
 
-    async create(data) {
-        let res = await firestore.collection(this._collection)
-        return res
-    }
+  async update(id, data) {
+    let doc = await firestore.collection(this._collection).doc(id)
+    return await doc.update(data)
+  }
 
-    async update(id,data) {
-        let doc = await firestore.collection(this._collection).doc(id)
-        let res = await doc.update(data)
-        return res
+  async getAll() {
+    let col = await firestore.collection(this._collection)
+    let res = await col.get()
+    let docArray = []
+    if (res.empty) {
+      return 'Não foram encontrados documentos!'
+    } else {
+      docArray = res.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      return docArray
     }
+  }
 
-    async getAll() {
-        let col = await firestore.collection(this._collection)
-        let res = await col.get()
-        let docArray = []
+  async getById(id) {
+    let doc = await firestore.collection(this._collection).doc(id)
+    let res = await doc.get()
+    if (!res.exists) {
+      return 'Não foi encontrado um documento correspondente ao ID informado!'
+    } else {
+      return res.data()
     }
+  }
+
+  async delete(id) {
+    return await firestore.collection(this._collection).doc(id).delete()
+  }
 }
+
+module.exports = repositoryBase
